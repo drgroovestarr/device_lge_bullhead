8f6ef9a7af4aee03eeda9452ffc2cf45239663fd
Jarrod Worlitz <jarrod.worlitz@gmail.com>
2016-12-16 03:00:45 -0800

    aosp_bullhead.mk: NMF26F build fingerprint

--- aosp_bullhead.mk
+++ aosp_bullhead.mk
@@ -37,3 +37,8 @@ $(call inherit-product-if-exists, vendor/lge/bullhead/device-vendor.mk)
 PRODUCT_PACKAGES += \
     Launcher3
 
+# Device Fingerprint
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=bullhead \
+    BUILD_FINGERPRINT=google/bullhead/bullhead:7.1.1/NMF26F/3425388:user/release-keys \
+    PRIVATE_BUILD_DESC="bullhead-user 7.1.1 NMF26F 3425388 release-keys"
