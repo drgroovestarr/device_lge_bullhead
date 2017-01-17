3b91e11045257c988f0d6c9d2192e1d9e378c504
razorloves <razorloves@gmail.com>
2017-01-16 22:51:50 -0600

    bullhead: switch to some blobs compiled from source
    Change-Id: I406656eebef58e5f07db697c77d76dd4ff2ba741

--- device.mk
+++ device.mk
@@ -207,6 +207,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     device/lge/bullhead/gps.conf:system/etc/gps.conf
 
+# GPS
+PRODUCT_PACKAGES += \
+    libgps.utils \
+    gps.msm8992
+
 # NFC packages
 PRODUCT_PACKAGES += \
     com.android.nfc_extras \
